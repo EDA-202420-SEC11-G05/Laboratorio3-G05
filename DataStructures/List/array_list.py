80c6c52e6982ea72c23adfb9c454af0db27e21a4
KJNegrette24 <k.negrette@uniandes.edu.co>
2024-08-21 17:00:41 -0500

    Array punto 11

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -5,3 +5,30 @@ def new_list():
     }
     return newlist
 
+def get_element(mylist, pos):
+    return mylist['elements'][pos-1]
+
+def is_present(mylist, element, cmp_function):
+    size = my_list["size"]
+    if size>0:
+        keyexist == false
+        for keypos in range (0,size):
+            info = mylist["elements"][keypos]
+            if (cmp_function(element, info) == 0):
+                keyexist == True
+                break
+        if keyexist:
+            return keypos
+    return -1
+
+def add_first():
+    pass
+
+def add_last():
+    pass
+
+def size():
+    pass
+
+def first_element():
+    pass
