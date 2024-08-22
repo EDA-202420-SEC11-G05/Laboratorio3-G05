5d7c52a18cac75dfc723cd580a730db63fa5aa44
juma2012 <jd.huertasc1@uniandes.edu.co>
2024-08-21 19:14:21 -0500

    Cambios en funciones

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -9,9 +9,9 @@ def get_element(mylist, pos):
     return mylist['elements'][pos-1]
 
 def is_present(mylist, element, cmp_function):
-    size = my_list["size"]
+    size = mylist["size"]
     if size>0:
-        keyexist == false
+        keyexist = False
         for keypos in range (0,size):
             info = mylist["elements"][keypos]
             if (cmp_function(element, info) == 0):
@@ -21,14 +21,15 @@ def is_present(mylist, element, cmp_function):
             return keypos
     return -1
 
-def add_first():
-    pass
+def add_first(mylist,element):
+    return set.add_element(mylist["elements"][0],element)
 
-def add_last():
-    pass
 
-def size():
-    pass
+def size(mylist):
+    return set.size(mylist["elements"])
 
-def first_element():
-    pass
+def add_last(mylist,element):
+    return set.add_element(mylist["elements"][size(mylist)-1],element)
+
+def first_element(mylist):
+    return set.get_first_element(mylist["elements"])
